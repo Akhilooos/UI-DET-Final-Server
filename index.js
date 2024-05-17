b0ae7a1eee3f80ee3a973579c9e2b92ab72e7ad0
unknown <akhilmathewmohan2023@cs.ajce.in>
2024-05-17 15:00:34 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import connectDb from "./db/dbconnect.js";
+import userrouter from "./routes/user-routes.js";
+import blogrouter from "./routes/blog-routes.js";
+
+const app = new express();
+
+app.use(express.json());
+
+app.use('/user',userrouter);
+app.use('/blog',blogrouter);
+ 
+const startServer = async() =>{
+try{
+        await app.listen(5500,
+            console.log("Connected to server"));
+        connectDb();
+}
+catch(error){
+    console.log(error);
+}
+
+}
+
+startServer();
+
